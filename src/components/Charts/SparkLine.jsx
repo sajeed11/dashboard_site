f40259767464a7e69f2b04babf6ec6189ef3dc84
Sadjed <77120200+sajeed11@users.noreply.github.com>
2023-10-08 23:42:04 +0100

    Fifth Day

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts';
 
-const SparkLine = () => {
-  return (
-    <div>SparkLine</div>
-  )
+export default class SparkLine extends React.PureComponent {
+  render() {
+    const { id, type, height, width, data, currentColor, color } = this.props;
+    return (
+      <SparklineComponent
+        id={id}
+        height={height}
+        width={width}
+        lineWidth={1}
+        valueType="Numeric"
+        fill={color}
+        border={{ color: currentColor, width: 2 }}
+        dataSource={data}
+        xName="xval"
+        yName="yval"
+        type={type}
+        tooltipSettings={{
+          visible: true,
+          format: '${xval} : data ${yval}',
+          trackLineSettings: {
+            visible: true
+          }
+        }}
+      >
+        <Inject services={[SparklineTooltip]} />
+      </SparklineComponent>
+    );
+  }
 }
-
-export default SparkLine
--- tailwind.config.js
+++ tailwind.config.js
@@ -39,7 +39,8 @@ module.exports = {
         590: "590px",
       },
       backgroundImage: {
-        bla: "./colorful-shopping-bags.jpg",
+        "hero-pattern":
+          "url('https://demos.wrappixel.com/premium-admin-templates/react/flexy-react/main/static/media/welcome-bg-2x-svg.25338f53.svg')",
       },
     },
   },
