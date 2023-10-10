4c1bb575e8628e5a4ea049350ad54af2d21b1bc5
Sadjed <77120200+sajeed11@users.noreply.github.com>
2023-10-10 21:54:56 +0100

    Seventh Day Edit

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -13,6 +13,21 @@ export const ContextProvider = ({ children }) => {
   const [activeMenu, setActiveMenu] = useState(true);
   const [isClicked, setIsClicked] = useState(initialiState);
   const [screenSize, setScreenSize] = useState(undefined);
+  const [currentColor, setCurrentColor] = useState("#03C907");
+  const [currentMode, setCurrentMode] = useState("Light");
+  const [themeSettings, setThemeSettings] = useState(false);
+
+  const setMode = (e) => {
+    setCurrentMode(e.target.value);
+
+    localStorage.setItem("themeMode", e.target.value);
+  };
+
+  const setColor = (color) => {
+    setCurrentColor(color);
+
+    localStorage.setItem("colorMode", color);
+  };
 
   const handleClick = (clicked) => {
     setIsClicked({ ...initialiState, [clicked]: true });
@@ -28,6 +43,14 @@ export const ContextProvider = ({ children }) => {
         handleClick,
         screenSize,
         setScreenSize,
+        currentColor,
+        currentMode,
+        setCurrentColor,
+        setCurrentMode,
+        setThemeSettings,
+        themeSettings,
+        setColor,
+        setMode,
       }}
     >
       {children}
